0c8f82a55c0013ef4c0f3467425cb6ea564afc37
Ari <rayarittri01@gmail.com>
2025-10-07 14:25:03 +1100

    Add files via upload

---
+++ week 11.py
@@ -0,0 +1,21 @@
+def main():
+    try:
+        infile = open("Salaries.txt", "r")
+        salary = int(infile.readline().strip())
+        print("Salary:", salary)
+
+    except FileNotFoundError:
+        print("File Salaries.txt not found.")
+
+    except ValueError:
+        print("File Salaries.txt contains an invalid salary.")
+
+    else:
+        infile.close()
+
+    finally:
+        print("Thank you for using our program.")
+
+
+if __name__ == "_main_":
+    main()
